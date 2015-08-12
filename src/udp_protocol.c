4c6c4adea27252a660a88e2cf8935f267b7ae325
August <augustt198@gmail.com>
2015-08-11 22:50:19 -0400

    Use better types

--- src/udp_protocol.c
+++ src/udp_protocol.c
@@ -8,32 +8,35 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define CONNECT_REQUEST_SIZE  16
-#define CONNECT_RESPONSE_SIZE 16
-#define ANNOUNCE_REQUEST_SIZE 100
+#define CONNECT_REQUEST_SIZE       16
+#define CONNECT_RESPONSE_SIZE      16
+#define ANNOUNCE_REQUEST_SIZE      100
 #define ANNOUNCE_RESPONSE_MIN_SIZE 20
 
 ssize_t send_connect_request(ConnectRequest *req,
     int sock, struct sockaddr *addr) {
 
     char buffer[CONNECT_REQUEST_SIZE];
-    *((long long *) (buffer)) = htonll(req->connection_id);
-    *((int*) (buffer + 8))    = htonl(req->action);
-    *((int*) (buffer + 12))   = htonl(req->transaction_id);
+    *((uint64_t*) (buffer)) = htonll(req->connection_id);
+    *((uint32_t*) (buffer + 8))    = htonl(req->action);
+    *((uint32_t*) (buffer + 12))   = htonl(req->transaction_id);
 
-    return sendto(sock, buffer, CONNECT_REQUEST_SIZE, 0, addr, sizeof(*addr));
+    if (addr == NULL) {
+        return send(sock, buffer, CONNECT_REQUEST_SIZE, 0);
+    } else {
+        return sendto(sock, buffer, CONNECT_REQUEST_SIZE, 0, addr, sizeof(*addr));
+    }
 }
 
 ssize_t receive_connect_response(ConnectResponse* res, int sock) {
-
     char buffer[CONNECT_RESPONSE_SIZE];
     ssize_t size = recvfrom(sock, buffer, CONNECT_RESPONSE_SIZE, 0, NULL, NULL);
     if (size < CONNECT_RESPONSE_SIZE)
         return size;
 
-    res->action         = ntohl(*((int*) buffer));
-    res->transaction_id = ntohl(*((int*) (buffer + 4)));
-    res->connection_id  = ntohll(*(long long *) (buffer + 8));
+    res->action         = ntohl(*((uint32_t*) buffer));
+    res->transaction_id = ntohl(*((uint32_t*) (buffer + 4)));
+    res->connection_id  = ntohll(*(uint64_t*) (buffer + 8));
 
     return size;
 }
@@ -44,22 +47,26 @@ ssize_t send_announce_request(AnnounceRequest *req,
     char buffer[ANNOUNCE_REQUEST_SIZE];
     memset(buffer, 0, ANNOUNCE_REQUEST_SIZE);
 
-    *((long long *) buffer) = htonll(req->connection_id);
-    *((int*) (buffer + 8))  = htonl(req->action);
-    *((int*) (buffer + 12)) = htonl(req->transaction_id);
+    *((uint64_t*) (buffer + 0))  = htonll(req->connection_id);
+    *((uint32_t*) (buffer + 8))  = htonl (req->action);
+    *((uint32_t*) (buffer + 12)) = htonl (req->transaction_id);
     memcpy(buffer + 16, req->info_hash, 20);
     memcpy(buffer + 36, req->peer_id, 20);
-    *((long long *) (buffer + 56)) = htonll(req->downloaded);
-    *((long long *) (buffer + 64)) = htonll(req->left);
-    *((long long *) (buffer + 72)) = htonll(req->uploaded);
-    *((int*) (buffer + 80))   = htonl(req->event);
-    *((int*) (buffer + 84))   = htonl(req->ip);
-    *((int*) (buffer + 88))   = htonl(req->key);
-    *((int*) (buffer + 92))   = htonl(req->num_want);
-    *((short*) (buffer + 96)) = htons(req->port);
-    *((short*) (buffer + 98)) = htons(req->extensions);
+    *((uint64_t*) (buffer + 56)) = htonll(req->downloaded);
+    *((uint64_t*) (buffer + 64)) = htonll(req->left);
+    *((uint64_t*) (buffer + 72)) = htonll(req->uploaded);
+    *((uint32_t*) (buffer + 80)) = htonl (req->event);
+    *((uint32_t*) (buffer + 84)) = htonl (req->ip);
+    *((uint32_t*) (buffer + 88)) = htonl (req->key);
+    *((uint32_t*) (buffer + 92)) = htonl (req->num_want);
+    *((uint16_t*) (buffer + 96)) = htons (req->port);
+    *((uint16_t*) (buffer + 98)) = htons (req->extensions);
 
-    return sendto(sock, buffer, ANNOUNCE_REQUEST_SIZE, 0, addr, sizeof(*addr));
+    if (addr == NULL) {
+        return send(sock, buffer, ANNOUNCE_REQUEST_SIZE, 0);
+    } else {
+        return sendto(sock, buffer, ANNOUNCE_REQUEST_SIZE, 0, addr, sizeof(*addr));
+    }
 }
 
 ssize_t receive_announce_response(AnnounceResponse *res, int sock) {
@@ -68,11 +75,11 @@ ssize_t receive_announce_response(AnnounceResponse *res, int sock) {
     if (size < ANNOUNCE_RESPONSE_MIN_SIZE)
         return size;
 
-    res->action         = ntohl(*((int*) buffer));
-    res->transaction_id = ntohl(*((int*) (buffer + 4)));
-    res->interval       = ntohl(*((int*) (buffer + 8)));
-    res->leechers       = ntohl(*((int*) (buffer + 12)));
-    res->seeders        = ntohl(*((int*) (buffer + 16)));
+    res->action         = ntohl(*((uint32_t*) (buffer + 0)));
+    res->transaction_id = ntohl(*((uint32_t*) (buffer + 4)));
+    res->interval       = ntohl(*((uint32_t*) (buffer + 8)));
+    res->leechers       = ntohl(*((uint32_t*) (buffer + 12)));
+    res->seeders        = ntohl(*((uint32_t*) (buffer + 16)));
 
     res->peer_count = (size - ANNOUNCE_RESPONSE_MIN_SIZE) / 6;
 
@@ -85,8 +92,8 @@ ssize_t receive_announce_response(AnnounceResponse *res, int sock) {
             peer->next = next_peer;
         }
 
-        next_peer->ip   = ntohl(*((int*) (buffer + i)));
-        next_peer->port = ntohs(*((int*) (buffer + i + 4)));
+        next_peer->ip   = ntohl(*((uint32_t*) (buffer + i + 0)));
+        next_peer->port = ntohs(*((uint32_t*) (buffer + i + 4)));
 
         peer = next_peer;
     }
